0dbc70a5e2a9abff761dfccfe66aa97fb70605e3
Josh Winters and Topher Bullock <jwinters@pivotal.io>
2015-06-23 12:35:25 -0400

    Changes the run-test script to use the debug build

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,7 +7,7 @@ source acceptanceconfig
 brew install node
 npm install -g ios-deploy
 
-ios-deploy --bundle ../data-ios-samples/build/Release-iphoneos/PCFDataSample-cal.app
+ios-deploy --bundle ../data-ios-samples/build/Debug-iphoneos/PCFDataSample-cal.app
 
 BUNDLE_ID=io.pivotal.ios.PCFDataSample-cal DEVICE_TARGET=9bb3f0712804e21a0cd4267175b171ba62275651 DEVICE_ENDPOINT=http://10.74.16.174:37265 cucumber
 
