7ede9cc4a341b115c1b46f6ae047638aa62bdc0f
Devin Fallak <dfallak@pivotal.io>
2015-07-31 09:55:34 -0400

    Change ios-deploy bundle path
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,7 +7,7 @@ $(dirname $0)/acceptance-setup.sh
 brew install node
 npm install -g ios-deploy
 
-ios-deploy --bundle ../data-ios-samples/build/Debug-iphoneos/PCFDataSample-cal.app
+ios-deploy --bundle $(dirname $0)/../build/Debug-iphoneos/PCFDataSample-cal.app
 
 BUNDLE_ID=io.pivotal.ios.PCFDataSample-cal DEVICE_TARGET=9bb3f0712804e21a0cd4267175b171ba62275651 DEVICE_ENDPOINT=http://10.74.16.174:37265 cucumber
 
