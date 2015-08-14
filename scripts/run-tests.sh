fd05d44327f6aa056ddf60a643d5436107112383
Gavin Enns <genns@pivotal.io>
2015-08-14 11:14:11 -0400

    [#100939106] Refactored the run-tests script. Moved everything into one script to avoid doing setup twice. Use env variables for ios device information instead of hardcoding them.
    Signed-off-by: Ergin Babani <ebabani@pivotal.io>

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,13 +1,165 @@
 #!/bin/bash
 
+# 1. Fetch token for admin
+# 2. Create user
+# 3. Create namespace
+# 4. Create collection
+# 5. Fetch Certificate
+# 6. Fetch token for user created in #2
+# 7. Write configuration files
+# 8. Run tests
+
 set -ex
 
-source $(dirname $0)/acceptance-setup.sh
+([ -z $UAA_ADMIN_IDENTITY ] || [ -z $UAA_ADMIN_PASSWORD ] || [ -z $UAA_URL ] || [ -z $SYSTEM_DOMAIN ]) && echo "Missing environment variables" && exit 1
+
+export USERNAME=$(uuidgen)
+export PASSWORD=$(uuidgen)
+export NAMESPACE=$(uuidgen)
+export COLLECTION=objects
+
+auth_url=https:\/\/datasync-authentication.$SYSTEM_DOMAIN
+data_url=https:\/\/datasync-datastore.$SYSTEM_DOMAIN
+
+echo ""
+echo "======================================================"
+echo "1. Fetch token for admin"
+echo "======================================================"
+echo ""
+
+authorization_header_uaa="Authorization: Basic $(printf "cf:" | base64)"
+payload_uaa="username=$UAA_ADMIN_IDENTITY&password=$UAA_ADMIN_PASSWORD&scope=openid&grant_type=password"
+
+admin_token=$(curl -sk $UAA_URL/oauth/token -X POST -H "$authorization_header_uaa" -d "$payload_uaa" | jq '.access_token' | awk -F '"' '{print $2}')
+
+echo ""
+echo "======================================================"
+echo "2. Create user"
+echo "======================================================"
+echo ""
+
+(
+content_type_header="Content-Type: application/json"
+authorization_header="Authorization: Bearer $admin_token"
+payload="{\"username\" : \"$USERNAME\", \"password\" : \"$PASSWORD\"}"
+
+curl -sk $auth_url/api/users -X POST -H "$authorization_header" -H "$content_type_header" -d "$payload"
+)
+
+echo ""
+echo "======================================================"
+echo "3. Create namespace"
+echo "======================================================"
+echo ""
+
+(
+authorization_header="Authorization: Bearer $admin_token"
+payload="{\"name\" : \"$NAMESPACE\"}"
+
+curl -sk $data_url/admin/namespaces -X POST -H "$authorization_header" -d "$payload"
+)
+
+echo ""
+echo "======================================================"
+echo "4. Create collection"
+echo "======================================================"
+echo ""
+
+(
+authorization_header="Authorization: Bearer $admin_token"
+payload="{\"name\" : \"$COLLECTION\"}"
+
+curl -sk $data_url/admin/namespaces/$NAMESPACE/collections -X POST -H "$authorization_header" -d "$payload"
+)
+
+echo ""
+echo "======================================================"
+echo "5. Fetch certificate"
+echo "======================================================"
+echo ""
+
+cert_path=$(dirname $0)/../PCFDataSample/cert.der
+
+$(dirname $0)/get-certificates.sh *.$SYSTEM_DOMAIN:443 $cert_path
+
+echo ""
+echo "======================================================"
+echo "6. Fetch token for user"
+echo "======================================================"
+echo ""
+
+client_id=ios-client
+client_secret=006d0cea91f01a82cdc57afafbbc0d26c8328964029d5b5eae920e2fdc703169
+payload_auth="username=$USERNAME&password=$PASSWORD&scope=openid&grant_type=password&client_id=$client_id&client_secret=$client_secret"
+
+access_token=$(curl -sk $auth_url/token -X POST -d "$payload_auth" | jq '.access_token' | awk -F '"' '{print $2}')
+
+echo $access_token
+
+echo ""
+echo "======================================================"
+echo "7. Write configuration files"
+echo "======================================================"
+echo ""
+
+cat > $(dirname $0)/../PCFDataSample/Pivotal.plist << EOM
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>pivotal.auth.scopes</key>
+  <string>openid offline_access</string>
+  <key>pivotal.auth.tokenUrl</key>
+  <string>$auth_url/token</string>
+  <key>pivotal.auth.authorizeUrl</key>
+  <string>$auth_url/authorize</string>
+  <key>pivotal.auth.redirectUrl</key>
+  <string>io.pivotal.ios.PCFDataSample://oauth2callback</string>
+  <key>pivotal.auth.clientId</key>
+  <string>$client_id</string>
+  <key>pivotal.auth.clientSecret</key>
+  <string>$client_secret</string>
+  <key>pivotal.data.serviceUrl</key>
+  <string>$data_url/data/$NAMESPACE</string>
+  <key>pivotal.data.collisionStrategy</key>
+  <string>OptimisticLocking</string>
+  <key>pivotal.data.trustAllSslCertificates</key>
+  <string>false</string>
+  <key>pivotal.auth.trustAllSslCertificates</key>
+  <string>false</string>
+  <key>pivotal.data.pinnedSslCertificateNames</key>
+  <string>$(basename $cert_path)</string>
+  <key>pivotal.auth.pinnedSslCertificateNames</key>
+  <string>$(basename $cert_path)</string>
+</dict>
+</plist>
+EOM
+
+echo ""
+echo "======================================================"
+echo "8. Build"
+echo "======================================================"
+echo ""
+
+gem install calabash-cucumber
+
+echo "
+
+y" | calabash-ios setup
+
+xcodebuild -sdk iphoneos -project PCFDataSample.xcodeproj -target PCFDataSample-cal -config Debug clean build PROVISIONING_PROFILE=$PROFILE_ID
+
+echo ""
+echo "======================================================"
+echo "9. Run"
+echo "======================================================"
+echo ""
+
 
 brew install node
 npm install -g ios-deploy
 
 ios-deploy --bundle $(dirname $0)/../build/Debug-iphoneos/PCFDataSample-cal.app
 
-BUNDLE_ID=io.pivotal.ios.PCFDataSample-cal DEVICE_TARGET=9bb3f0712804e21a0cd4267175b171ba62275651 DEVICE_ENDPOINT=http://10.74.16.174:37265 cucumber
+BUNDLE_ID=io.pivotal.ios.PCFDataSample-cal DEVICE_TARGET=$DEVICE_UDID DEVICE_ENDPOINT=http://10.74.16.174:37265 cucumber
 
